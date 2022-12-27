ef7a4a283dd5e12a1f4465fe38ee55b2f13af574
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-27 14:55:09 -0500

    created queries_controller.rb + query creation works in rails c

---
+++ app/controllers/queries_controller.rb
@@ -0,0 +1,11 @@
+class QueriesController < ApplicationController
+  def create
+    query = Query.create(query_params)
+  end
+
+  private
+
+  def query_params
+    params.require(:query).permit(:query) # => {query: 'something'}
+  end
+end
