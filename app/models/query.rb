9c9ce79a3d03b954d23164777128dd10cad22ee4
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-27 14:48:59 -0500

    created query.rb

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,2 +1,13 @@
 class Query < ApplicationRecord
+  #validations
+  validates_presence_of :query
+  validates_length_of :query, minimum: 1
+
+  #callback
+  after_create :generate_name
+
+  # send query text to OpenAI and get the result back and update the query object with the generated name
+  def generate_name
+    puts "Yo name generated!"
+  end
 end
